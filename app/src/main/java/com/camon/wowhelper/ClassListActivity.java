4a2b5a2fbe07ee795177cec6cb96a789dd5caa8c
joo-yong.sung <camon85@gmail.com>
2014-12-18 17:56:33 +0900

    Initial commit

---
+++ app/src/main/java/com/camon/wowhelper/ClassListActivity.java
@@ -0,0 +1,87 @@
+package com.camon.wowhelper;
+
+import android.app.ListActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class ClassListActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_list);
+
+        // ArrayAdapter를 통해 LIST로 표시할 오브젝트를 지정한다.
+        // 여기서는 심플하게 그냥 String
+        // 레이아웃 android.R.layout.simple_list_item_1 는 안드로이드가 기본적으로 제공하는 간단한 아이템 레이아웃
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+        // 아이템을 추가
+        adapter.add("item1");
+        adapter.add("item2");
+        adapter.add("item3");
+
+        // ListView 가져오기
+        ListView listView = (ListView) findViewById(R.id.list_class);
+
+        // ListView에 각각의 아이템표시를 제어하는 Adapter를 설정
+        listView.setAdapter(adapter);
+
+        // 아이템을 [클릭]시의 이벤트 리스너를 등록
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ListView listView = (ListView) parent;
+                // TODO 아이템 클릭시에 구현할 내용은 여기에.
+                String item = (String) listView.getItemAtPosition(position);
+                Toast.makeText(ClassListActivity.this, item, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // 아이템을 [선택]시의 이벤트 리스너를 등록
+        listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                ListView listView = (ListView) parent;
+                // TODO 아이템 선택시에 구현할 내용은 여기에.
+                String item = (String) listView.getSelectedItem();
+                Toast.makeText(ClassListActivity.this, item, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_class_list, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
